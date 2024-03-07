7fc33414b74e19955b9db5e6fd0bfb991ccd2298
umtdemr <umitde692@gmail.com>
2024-03-07 21:43:54 +0300

    add date and session select

--- main.go
+++ main.go
@@ -1,12 +1,52 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package main
 
 import (
 	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"os"
+	"time"
 )
 
+type model struct {
+	view            string
+	choices         []time.Time
+	sessionChoices  []string
+	cursor          int
+	selectedDate    time.Time
+	selectedSession string
+}
+
+func initialModel() model {
+	return model{
+		// Our to-do list is a grocery list
+		choices:        []time.Time{time.Now(), time.Now().Add(time.Hour * 24), time.Now().Add(time.Hour * 48)},
+		sessionChoices: []string{"8:30 - 14.30", "17.00 - 22.00"},
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if m.view == "sessions" {
+		return m.UpdateSessions(msg)
+	}
+	return m.UpdateDates(msg)
+}
+
+func (m model) View() string {
+	if m.view == "sessions" {
+		return m.SessionSelectView()
+	}
+	return m.DateSelectView()
+}
+
 func main() {
-	fmt.Println("hello")
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
---
+++ session.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func (m model) UpdateSessions(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.sessionChoices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			m.selectedSession = m.sessionChoices[m.cursor]
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) SessionSelectView() string {
+	// The header
+	s := fmt.Sprintf("Selected Date: %s,\nNow, please select the session you want to reserve\n\n", m.selectedDate.Format("02/01/2006"))
+
+	// Iterate over our choices
+	for i, choice := range m.sessionChoices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if m.selectedSession == choice {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
+}
