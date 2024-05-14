380753e8f54e426e0589fc0fbd15536f4f6da845
umtdemr <umitde692@gmail.com>
2024-05-14 20:59:11 +0300

    panic on login client err

--- internal/client/login.go
+++ internal/client/login.go
@@ -13,7 +13,11 @@ func (c *Client) Login(id string, password string) *bytes.Buffer {
 	formData.Set("btnGirisYap", "Giriş Yap")
 	formData.Set("__VIEWSTATE", "18v9/jvlC8qsN16XpBUmSb1Pq4Qp4X0pMErF1AMS0Kw/METmb6YGeh04udRG+fyrUGWFjPMGPETZp7235nCmqmDNRkAlboNzDmgy7etyxJcHXpwBY1+pxMTfnOTlOsz/")
 
-	resp, _ := c.HttpClient.PostForm(c.BaseURL+LOGIN_URL, formData)
+	resp, postErr := c.HttpClient.PostForm(c.BaseURL+LOGIN_URL, formData)
+
+	if postErr != nil {
+		panic(postErr.Error())
+	}
 
 	defer resp.Body.Close()
 
